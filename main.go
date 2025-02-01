3e81f7e075aea8ea0cf151522579ad214c678bd0
Mamun172136 <ma1536204@gmail.com>
2025-02-02 02:00:47 +0600

    added note struct

--- main.go
+++ main.go
@@ -1,36 +1,41 @@
 package main
 
 import (
-	"errors"
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+
+	"example.com/note/note"
 )
 
 func main() {
-	title, content,err := getNoteData()
-	if err != nil{
-		fmt.Println(err)
-		return
-	}
+	title, content := getNoteData()
+ userNote,err:=	note.New(title,content)
+
+ if err!= nil{
+	fmt.Println(err)
+ }
+ userNote.Display()
 }
 
-func getNoteData()(string,string, error){
-	title,err := getUserInput("Note title")
-	if err != nil{
-		return "","",err
-	}
-	content ,err := getUserInput("Note content")
-	if err != nil{
-		return "","",err
-	}
-	return title, content, err
+func getNoteData()(string,string){
+	title := getUserInput("Note title")
+	
+	content := getUserInput("Note content")
+	
+	return title, content
 }
 
-func getUserInput(propt string) (string,error){
+func getUserInput(propt string) string{
 	fmt.Println(propt)
-	var value string
-	fmt.Scanln(&value)
-	if value == ""{
-		return "", errors.New("invalid empty string")
+	 reader :=bufio.NewReader(os.Stdin)
+	text,err :=reader.ReadString('\n')
+
+	if err != nil{
+		return ""
 	}
-	return value,nil
+	text = strings.TrimSuffix(text,"\n")
+	text = strings.TrimSuffix(text,"\r")
+	return text
 }
---
+++ note/note.go
@@ -0,0 +1,28 @@
+package note
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type Note struct {
+	title     string
+	content   string
+	createdAt time.Time
+}
+
+func New(title, content string)( Note,error){
+	if(title =="" || content == ""){
+		return Note{}, errors.New("")
+	}
+	return Note {
+		title:title,
+		content: content,
+		createdAt: time.Now(),
+	},nil
+}
+
+func (note Note)Display(){
+	fmt.Printf("your note has titled %v has the following content\n\n%v",note.title,note.content)
+}
